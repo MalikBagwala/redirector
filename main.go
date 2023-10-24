61cffc5649fa61a8dc12ceda7e76cb6af7e18b6a
Malik Bagwala <m.bagwala@gmail.com>
2023-10-24 13:28:22 +0530

    feat: add template support in go

--- main.go
+++ main.go
@@ -1,40 +1,55 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 
 	"github.com/joho/godotenv"
 	"github.com/schollz/closestmatch"
 	"golang.org/x/exp/maps"
 )
 
-func redirectHandler(redirectMap map[string]string, cm *closestmatch.ClosestMatch) http.HandlerFunc {
+func redirectHandler(redirectMap map[string]string, cm *closestmatch.ClosestMatch, tmpl *template.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "public, max-age=600")
 		// Get the requested path from the URL
 		requestedPath := r.URL.Path
-		// If the requested path is "/", return the redirect map as JSON
+
+		// If the requested path is "/", serve the HTML template
 		if requestedPath == "/" {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(redirectMap)
+
+			w.Header().Set("Content-Type", "text/html")
+			data := struct {
+				RedirectMap map[string]string
+			}{
+				RedirectMap: redirectMap,
+			}
+			err := tmpl.Execute(w, data)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
 			return
 		}
-		// Find the closest path
-		// eg. /insta -> /instagram
-		// eg. /intagram -> /instagram
+		// Find the closest path and perform redirection
 		closestPath := cm.Closest(requestedPath)
 		http.Redirect(w, r, redirectMap[closestPath], http.StatusFound)
 	}
 }
+
 func main() {
 	// Load environment variables from the .env file
 	if err := godotenv.Load(); err != nil {
 		log.Println("Warning: Error loading .env file:", err)
 	}
+	// Parse the HTML template
+	tmpl, teplErr := template.ParseFiles("template.html")
+	if teplErr != nil {
+		log.Fatal(teplErr)
+	}
+
 	redirectMap := map[string]string{
 		"/instagram":     os.Getenv("INSTAGRAM"),
 		"/linkedin":      os.Getenv("LINKEDIN"),
@@ -58,7 +73,7 @@ func main() {
 	cm := closestmatch.New(wordsToTest, []int{1})
 
 	// Create a new HTTP server and set up the redirectHandler for all routes
-	http.HandleFunc("/", redirectHandler(redirectMap, cm))
+	http.HandleFunc("/", redirectHandler(redirectMap, cm, tmpl))
 
 	// Specify the port to listen on
 	port := os.Getenv("PORT")
